7b4379b2c4bb8c5f1f7becdb48fa7cca71d36df4
zjun <565061763@qq.com>
2021-06-15 10:11:56 +0800

    demo

--- app/src/main/java/com/sd/demo/retry/MainActivity.kt
+++ app/src/main/java/com/sd/demo/retry/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.sd.lib.retry.demo
+package com.sd.demo.retry
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
--- app/src/main/java/com/sd/demo/retry/MyRetryHandler.kt
+++ app/src/main/java/com/sd/demo/retry/MyRetryHandler.kt
@@ -1,4 +1,4 @@
-package com.sd.lib.retry.demo
+package com.sd.demo.retry
 
 import android.content.Context
 import android.util.Log
