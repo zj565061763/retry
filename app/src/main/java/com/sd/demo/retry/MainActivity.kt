71e64b175846b61ead2eb58f077ccc596a29e135
zjun <565061763@qq.com>
2021-06-15 11:13:30 +0800

    demo

--- app/src/main/java/com/sd/demo/retry/MainActivity.kt
+++ app/src/main/java/com/sd/demo/retry/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.sd.demo.retry
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.sd.demo.retry.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var _binding: ActivityMainBinding
     private lateinit var _retryHandler: MyRetryHandler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
         _retryHandler = MyRetryHandler(this)
-        _retryHandler.start()
+    }
+
+    override fun onClick(v: View) {
+        when (v) {
+            _binding.btnStart -> _retryHandler.start()
+            _binding.btnStop -> _retryHandler.cancel()
+        }
     }
 
     override fun onDestroy() {
