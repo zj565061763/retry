e0b425223d4283a98fedabe46507b462f97b4915
zjun <565061763@qq.com>
2021-06-15 09:48:11 +0800

    demo

---
+++ app/src/main/java/com/sd/lib/retry/demo/MyRetryHandler.kt
@@ -0,0 +1,36 @@
+package com.sd.lib.retry.demo
+
+import android.content.Context
+import android.util.Log
+import com.sd.lib.retry.FNetRetryHandler
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MyRetryHandler : FNetRetryHandler {
+    private val TAG = MyRetryHandler::class.java.simpleName
+    private var _count = 0
+
+    constructor(context: Context) : super(context, 15) {
+
+    }
+
+    override fun onRetry(session: LoadSession) {
+        session.onLoading()
+        GlobalScope.launch {
+            delay(1000)
+            _count++
+            Log.i(TAG, "onRetry count:${_count}")
+            if (_count >= 10) {
+                session.onLoadFinish()
+            } else {
+                session.onLoadError()
+            }
+        }
+    }
+
+    override fun onRetryMaxCount() {
+        super.onRetryMaxCount()
+        Log.i(TAG, "onRetryMaxCount")
+    }
+}
