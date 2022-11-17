ad08eec2e41ae67e5a40df7cdee4ffd493025e17
565061763 <565061763@qq.com>
2022-11-17 21:44:37 +0800

    NetworkObserver

--- lib/src/main/java/com/sd/lib/retry/NetworkObserver.kt
+++ lib/src/main/java/com/sd/lib/retry/NetworkObserver.kt
@@ -3,30 +3,95 @@ package com.sd.lib.retry
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
+import androidx.annotation.RequiresApi
+import java.util.concurrent.atomic.AtomicBoolean
 
-class NetworkObserver(context: Context) {
+abstract class NetworkObserver(context: Context) {
     private val _context = context.applicationContext
 
-    val isNetworkAvailable
-        get() = isNetworkAvailable(_context)
+    private val _networkReceiver by lazy { NetworkReceiver() }
+    private val _networkCallback by lazy { InternalNetworkCallback() }
 
-    fun register() {}
+    private var _isNetworkAvailable: Boolean = isNetworkAvailable(context)
+        set(value) {
+            if (field != value) {
+                field = value
+                onNetworkChanged(value)
+            }
+        }
 
-    fun unregister() {}
+    val isNetworkAvailable: Boolean
+        get() = _isNetworkAvailable
+
+    fun register() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            _networkCallback.register()
+        } else {
+            _networkReceiver.register()
+        }
+    }
+
+    fun unregister() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            _networkCallback.unregister()
+        } else {
+            _networkReceiver.unregister()
+        }
+    }
+
+    abstract fun onNetworkChanged(isNetworkAvailable: Boolean)
+
+    private inner class InternalNetworkCallback : ConnectivityManager.NetworkCallback() {
+        private val _hasRegister = AtomicBoolean()
+
+        @RequiresApi(Build.VERSION_CODES.N)
+        fun register() {
+            if (_hasRegister.compareAndSet(false, true)) {
+                val manager = _context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+                manager.registerDefaultNetworkCallback(this)
+            }
+        }
+
+        fun unregister() {
+            if (_hasRegister.compareAndSet(true, false)) {
+                val manager = _context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+                manager.unregisterNetworkCallback(this)
+            }
+        }
+    }
 
     private inner class NetworkReceiver : BroadcastReceiver() {
+        private val _hasRegister = AtomicBoolean()
+
         override fun onReceive(context: Context, intent: Intent) {
-            TODO("Not yet implemented")
+            if (ConnectivityManager.CONNECTIVITY_ACTION == intent.action) {
+                _isNetworkAvailable = isNetworkAvailable(context)
+            }
+        }
+
+        fun register() {
+            if (_hasRegister.compareAndSet(false, true)) {
+                _context.registerReceiver(this, IntentFilter().apply {
+                    addAction(ConnectivityManager.CONNECTIVITY_ACTION)
+                })
+            }
+        }
+
+        fun unregister() {
+            if (_hasRegister.compareAndSet(true, false)) {
+                _context.unregisterReceiver(this)
+            }
         }
     }
 
     companion object {
         private fun isNetworkAvailable(context: Context): Boolean {
             val manager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 val network = manager.activeNetwork ?: return false
                 val capabilities = manager.getNetworkCapabilities(network) ?: return false
                 return when {
