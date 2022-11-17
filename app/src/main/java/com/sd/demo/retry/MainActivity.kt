6eff280c3f505e57268ac85070b0701a695eaab2
565061763 <565061763@qq.com>
2022-11-18 01:07:37 +0800

    demo

--- app/src/main/java/com/sd/demo/retry/MainActivity.kt
+++ app/src/main/java/com/sd/demo/retry/MainActivity.kt
@@ -30,15 +30,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        logMsg { "onResume isNetworkAvailable:${FNetworkObserver.isNetworkAvailable(this)}" }
-    }
-
     override fun onDestroy() {
         super.onDestroy()
         _retry.cancel()
         _networkObserver.unregister()
+        logMsg { "onDestroy isNetworkAvailable:${FNetworkObserver.isNetworkAvailable(this)}" }
     }
 }
 
