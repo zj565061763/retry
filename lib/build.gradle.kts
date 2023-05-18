e8eb6d9cd9624017336415048081aeb6876ce199
zj565061763 <565061763@qq.com>
2023-05-18 08:25:39 +0800

    format

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -18,7 +18,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-
     }
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
