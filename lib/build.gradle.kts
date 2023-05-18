4508c435111895b712250a37918c7510ef4e65c2
zj565061763 <565061763@qq.com>
2023-05-19 02:25:48 +0800

    1.3.0-beta03

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "retry"
-val libVersionName = "1.3.0-beta02"
+val libVersionName = "1.3.0-beta03"
 
 android {
     namespace = "com.sd.lib.retry"
