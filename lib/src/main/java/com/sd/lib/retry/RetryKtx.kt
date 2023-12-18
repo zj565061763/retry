7e053c27912bb3dd3503c0e31e7c5dcb7e510fd6
zjun <565061763@qq.com>
2023-12-18 22:53:08 +0800

    fRetry

--- lib/src/main/java/com/sd/lib/retry/RetryKtx.kt
+++ lib/src/main/java/com/sd/lib/retry/RetryKtx.kt
@@ -1,9 +1,46 @@
 package com.sd.lib.retry
 
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.ensureActive
+
 suspend fun <T> fRetry(
-    maxRetryCount: Int = Int.MAX_VALUE,
-    retryInterval: Long = 3000,
+    /** 最多执行几次 */
+    maxCount: Int = Int.MAX_VALUE,
+
+    /** 执行间隔（毫秒） */
+    interval: Long = 3_000,
+
+    /** [block]执行之前调用 */
+    check: suspend () -> Unit = { },
+
+    /** 执行回调 */
     block: suspend () -> Result<T>,
 ): Result<T> {
-    return Result.failure(Exception())
+    require(maxCount > 0)
+
+    var count = 0
+    while (true) {
+        count++
+        check()
+
+        currentCoroutineContext().ensureActive()
+        val result = block()
+        currentCoroutineContext().ensureActive()
+
+        if (result.isSuccess) {
+            return result
+        }
+
+        if (count >= maxCount) {
+            return Result.failure(FRetryExceptionRetryMaxCount())
+        }
+
+        delay(interval)
+    }
 }
+
+/**
+ * 达到最大重试次数
+ */
+class FRetryExceptionRetryMaxCount : Exception()
