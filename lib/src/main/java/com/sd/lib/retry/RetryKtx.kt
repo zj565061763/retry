dac82fc876130f44b2007e3b4373b2d9239ec3d7
zjun <565061763@qq.com>
2023-12-18 22:55:44 +0800

    fNetRetry

--- lib/src/main/java/com/sd/lib/retry/RetryKtx.kt
+++ lib/src/main/java/com/sd/lib/retry/RetryKtx.kt
@@ -1,8 +1,33 @@
 package com.sd.lib.retry
 
+import com.sd.lib.network.fAwaitNetworkAvailable
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.ensureActive
+
+suspend fun <T> fNetRetry(
+    /** 最多执行几次 */
+    maxCount: Int = Int.MAX_VALUE,
+
+    /** 执行间隔（毫秒） */
+    interval: Long = 3_000,
+
+    /** [block]执行之前调用 */
+    check: suspend () -> Unit = { },
+
+    /** 执行回调 */
+    block: suspend () -> Result<T>,
+): Result<T> {
+    return fRetry(
+        maxCount = maxCount,
+        interval = interval,
+        check = {
+            fAwaitNetworkAvailable()
+            check()
+        },
+        block = block,
+    )
+}
 
 suspend fun <T> fRetry(
     /** 最多执行几次 */
