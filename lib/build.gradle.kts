c60c716ecab3bf60df28de22563e79cf800a6395
zjun <565061763@qq.com>
2024-03-16 14:20:20 +0800

    1.5.1

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "retry"
-val libVersionName = "1.5.0"
+val libVersionName = "1.5.1"
 
 android {
     namespace = "com.sd.lib.retry"
