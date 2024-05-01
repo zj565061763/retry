664331744bcfa62e4093787a25da3e04c389794a
zjun <565061763@qq.com>
2024-05-01 23:24:48 +0800

    1.9.0

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "retry"
-val libVersion = "1.9.0-beta01"
+val libVersion = "1.9.0"
 
 android {
     namespace = "com.sd.lib.retry"
